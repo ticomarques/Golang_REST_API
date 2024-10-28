156c14a62a0e59b40c4f87a1dfcbe70a9e260009
Tiago Marques <tiagomarques@Tiagos-MBP.lan>
2024-10-27 17:58:04 -1000

    Adding Save method and Fetch method on accessing the DB

--- main.go
+++ main.go
@@ -21,7 +21,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the events!"})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -37,7 +41,11 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save the event!"})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created sucessfully!", "event": event})
 }
